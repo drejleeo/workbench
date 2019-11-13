f25202857e853075fb4c57d1f554935621a922e4
aleonte <aleonte@pentalog.com>
2019-11-13 18:24:10 +0200

    Update on tiling problem; still not finished.

--- algo/dei/tiling_problem.py
+++ algo/dei/tiling_problem.py
@@ -13,33 +13,56 @@ def can_be_placed(board, tile):
     pass
 
 
-def get_markerd_quarter(quarters):
+def get_marked_quarter(quarters):
     for key in quarters.keys():
         if sum(sum(row) for row in quarters[key]) == 1:
             return key
 
 
-def solve_tiling(board):
-    lng = dim
-    mid = lng // 2
+def place_tile(already_marked, tile_number, board, half):
+    spots = {
+        'wn': board[half-1][half-1],
+        'ne': board[half-1][ half ],
+        'es': board[ half ][half-1],
+        'sw': board[ half ][ half ],
+    }
+    for key in spots.keys() - already_marked:
+        spots[key] = tile_number
 
+
+def solve_tiling(board, size, tile_number=1):
+
+    if size == 2:
+        return board
+
+    half = size // 2
     quarters = {
-        'wn': [row[0:mid] for row in board[0:mid]],
-        'ne': [row[mid:lng] for row in board[0:mid]],
-        'es': [row[0:mid] for row in board[mid:lng]],
-        'sw': [row[mid:lng] for row in board[mid:lng]],
+        'wn': [row[0:half] for row in board[0:half]],
+        'ne': [row[half:size] for row in board[0:half]],
+        'es': [row[0:half] for row in board[half:size]],
+        'sw': [row[half:size] for row in board[half:size]],
     }
 
-    mark = get_markerd_quarter(quarters)
-
+    mark = get_marked_quarter(quarters)
+    place_tile(
+        already_marked=mark,
+        tile_number=tile_number,
+        board=board,
+        half=half,
+    )
 
     for quart in quarters:
-        solve_tiling(quart)
+        solve_tiling(quart, half, tile_number + 1)
+
+
 
 
 if __name__ == '__main__':
     board = [[0] * dim for _ in range(dim)]
     board[random.randint(0, dim - 1)][random.randint(0, dim - 1)] = 1
-    result = solve_tiling(board)
-    print()
+    result = solve_tiling(
+        board=board,
+        size=dim,
+    )
+
     print_matrix(board)
